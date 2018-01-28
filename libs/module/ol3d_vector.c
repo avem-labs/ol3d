dd9761a0ed2a569826221c6d777cc8c7dca4a65d
but0n <but0n@icloud.com>
2018-01-28 14:38:40 +0800

    feat: (Vector3) Length and normalize

--- libs/module/ol3d_vector.c
+++ libs/module/ol3d_vector.c
@@ -9,9 +9,13 @@ ol3d_Vector3_t ol3d_vector_dot(ol3d_Vector3_t *a, ol3d_Vector3_t *b) {
     };
 }
 
-float ol3d_vector_normalize(ol3d_Vector3_t *a) {
+float ol3d_vector_length(ol3d_Vector3_t *a) {
     ol3d_Vector3_t temp = ol3d_vector_dot(a, a);
     return (float)sqrt(temp.x + temp.y + temp.z);
+}
+
+ol3d_Vector3_t ol3d_vector_normalize(ol3d_Vector3_t *a) {
+    return ol3d_vector_divide(a, ol3d_vector_length(a));
 }
 
 ol3d_Vector3_t ol3d_vector_add(ol3d_Vector3_t *a, ol3d_Vector3_t *b) {
@@ -27,6 +31,14 @@ ol3d_Vector3_t ol3d_vector_subtract(ol3d_Vector3_t *a, ol3d_Vector3_t *b) {
         .x = a->x - b->x,
         .y = a->y - b->y,
         .z = a->z - b->z
+    };
+}
+
+ol3d_Vector3_t ol3d_vector_multiply(ol3d_Vector3_t *a, float b) {
+    return (ol3d_Vector3_t) {
+        .x = a->x * b,
+        .y = a->y * b,
+        .z = a->z * b
     };
 }
 
--- libs/module/ol3d_vector.h
+++ libs/module/ol3d_vector.h
@@ -8,9 +8,11 @@ typedef struct {
 } ol3d_Vector3_t;
 
 extern ol3d_Vector3_t   ol3d_vector_dot(ol3d_Vector3_t *a, ol3d_Vector3_t *b);
-extern float            ol3d_vector_normalize(ol3d_Vector3_t *a);
+extern float            ol3d_vector_length(ol3d_Vector3_t *a);
+extern ol3d_Vector3_t   ol3d_vector_normalize(ol3d_Vector3_t *a);
 extern ol3d_Vector3_t   ol3d_vector_add(ol3d_Vector3_t *a, ol3d_Vector3_t *b);
 extern ol3d_Vector3_t   ol3d_vector_subtract(ol3d_Vector3_t *a, ol3d_Vector3_t *b);
+extern ol3d_Vector3_t   ol3d_vector_multiply(ol3d_Vector3_t *a, float b);
 extern ol3d_Vector3_t   ol3d_vector_divide(ol3d_Vector3_t *a, float b);
 
 
