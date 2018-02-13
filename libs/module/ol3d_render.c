f1b05c8ed5d0392c871dc0b953d0950e66ab3ecd
but0n <but0n@icloud.com>
2018-02-13 12:14:05 +0800

    Enhance(render)

--- libs/module/ol3d_render.c
+++ libs/module/ol3d_render.c
@@ -8,7 +8,7 @@ ol3d_buffer_t *render_target = &render_buffer;
 #define AABB_MIN(x, y, z)   (fmin((x), fmin((y), (z))))
 #define AABB_MAX(x, y, z)   (fmax((x), fmax((y), (z))))
 #define isLeft(_a, _b, _p)  (((_b->x-_a->x) * (_p->y-_a->y) - (_b->y-_a->y) * (_p->x-_a->x))>0)
-#define _UV(x)             (BUFFER_SIZE * ((x) + 1.0) * 0.5)
+#define COORD(x)            (BUFFER_SIZE * ((x) + 1.0) * 0.5)
 static unsigned char inTriangle(
     ol3d_Vector3_t *a,
     ol3d_Vector3_t *b,
@@ -21,14 +21,13 @@ static unsigned char inTriangle(
         .y = y
     };
 
-    if(isLeft(a, b, (&p)) != isLeft(b, c, (&p))) {
+    if(isLeft(a, b, (&p)) != isLeft(b, c, (&p)))
         return 0;
-    }
-    if(isLeft(a, b, (&p)) != isLeft(c, a, (&p))) {
+    if(isLeft(a, b, (&p)) != isLeft(c, a, (&p)))
         return 0;
-    }
     return 1;
 }
+
 void ol3d_draw_Triangle(
     ol3d_buffer_t target,
     ol3d_Vector3_t *a,
@@ -38,27 +37,28 @@ void ol3d_draw_Triangle(
     ol3d_Vector3_t *color_b,
     ol3d_Vector3_t *color_c
 ) {
+    a->x = COORD(a->x);
+    a->y = COORD(-a->y);
+    a->z = COORD(a->z);
+
+    b->x = COORD(b->x);
+    b->y = COORD(-b->y);
+    b->z = COORD(b->z);
+
+    c->x = COORD(c->x);
+    c->y = COORD(-c->y);
+    c->z = COORD(c->z);
+
     unsigned int minX, minY, maxX, maxY;
-    minX = (unsigned int)(BUFFER_SIZE * (AABB_MIN(a->x, b->x, c->x) + 1.0) * 0.5);
-    maxX = (unsigned int)(BUFFER_SIZE * (AABB_MAX(a->x, b->x, c->x) + 1.0) * 0.5);
-    minY = (unsigned int)(BUFFER_SIZE * (AABB_MIN(a->y, b->y, c->y) + 1.0) * 0.5);
-    maxY = (unsigned int)(BUFFER_SIZE * (AABB_MAX(a->y, b->y, c->y) + 1.0) * 0.5);
+    minX = (unsigned int)(AABB_MIN(a->x, b->x, c->x));
+    maxX = (unsigned int)(AABB_MAX(a->x, b->x, c->x));
+    minY = (unsigned int)(AABB_MIN(a->y, b->y, c->y));
+    maxY = (unsigned int)(AABB_MAX(a->y, b->y, c->y));
 
-    a->x = _UV(a->x);
-    a->y = _UV(-a->y);
-    a->z = _UV(a->z);
-
-    b->x = _UV(b->x);
-    b->y = _UV(-b->y);
-    b->z = _UV(b->z);
-
-    c->x = _UV(c->x);
-    c->y = _UV(-c->y);
-    c->z = _UV(c->z);
 
     for(unsigned int y = minY; y < maxY; y++) {
         for(unsigned int x = minX; x < maxX; x++) {
-            if(inTriangle(a, b, c, x, y) == 1) {
+            if(inTriangle(a, b, c, x, y)) {
                 target[x][y] = 1;
             }
         }
